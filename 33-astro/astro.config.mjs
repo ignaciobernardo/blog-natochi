37403eaf13e0714a1524d0a940bd6d267e1e038f
Ernesto Ignacio Bernardo <ignacioramosbernardo@gmail.com>
2026-09-23 17:24:46 -0300

    Add illustrated pages 33 and 34 with light themes

---
+++ 33-astro/astro.config.mjs
@@ -0,0 +1,2 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({site:'https://natochi.cv',base:'/33',outDir:'../33',build:{format:'directory'}});
---
+++ 34-astro/astro.config.mjs
@@ -0,0 +1,2 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({site:'https://natochi.cv',base:'/34',outDir:'../34',build:{format:'directory'}});
